8c240395cdd50a2ba27404856a60be0bcd9f7948
Max Lapshin <max@maxidoors.ru>
2010-03-23 12:03:49 +0300

    measure

--- bm.erl
+++ bm.erl
@@ -2,35 +2,42 @@
 %%! -pa ebin
 
 
+measure(Client, Server, Count) ->
+  Measures = 5,
+  measure(Client, Server, Count, Measures, []).
+  
+measure(_Client, _Server, _Count, 0, Times) ->
+  Sum = lists:foldl(fun(X, Sum) -> X + Sum end, 0, Times),
+  round(Sum / length(Times), round(_Count / 10));
+  
+measure(Client, Server, Count, Measures, Times) ->
+  {Time, _} = timer:tc(Client, bm, [Server, Count]),
+  measure(Client, Server, Count, Measures - 1, [Time|Times]).
+
+
+round(Num, Digits) ->
+  round(Num / Digits) * Digits.
+
 main([]) ->
   {ok, GenServer} = my_gen_server:start_link(),
   {ok, MyServer1} = my_server1:start_link(),
   {ok, MyServer2} = my_server2:start_link(),
-  Count = 1000000,
+  Count = 100000,
   
-  {TimeGG, _} = timer:tc(my_gen_client, bm, [GenServer, Count]),
-  io:format("Gen-Gen: ~p~n", [TimeGG]),
+  measure(my_gen_client, GenServer, round(Count/10)), % Warm up beam
+  
+  io:format("Gen-Gen: ~p~n", [measure(my_gen_client, GenServer, Count)]),
+  io:format("Gen-My1: ~p~n", [measure(my_gen_client, MyServer1, Count)]),
+  io:format("Gen-My2: ~p~n", [measure(my_gen_client, MyServer2, Count)]),
 
-  {TimeMG, _} = timer:tc(my_client, bm, [GenServer, Count]),
-  io:format("My-Gen: ~p~n", [TimeMG]),
+  io:format("My1-Gen: ~p~n", [measure(my_client1, GenServer, Count)]),
+  io:format("My1-My1: ~p~n", [measure(my_client1, MyServer1, Count)]),
+  io:format("My1-My2: ~p~n", [measure(my_client1, MyServer2, Count)]),
 
-  {TimeGM1, _} = timer:tc(my_gen_client, bm, [MyServer1, Count]),
-  io:format("Gen-My1: ~p~n", [TimeGM1]),
+  io:format("My2-Gen: ~p~n", [measure(my_client2, GenServer, Count)]),
+  io:format("My2-My1: ~p~n", [measure(my_client2, MyServer1, Count)]),
+  io:format("My2-My2: ~p~n", [measure(my_client2, MyServer2, Count)]),
 
-  {TimeGM2, _} = timer:tc(my_gen_client, bm, [MyServer2, Count]),
-  io:format("Gen-My2: ~p~n", [TimeGM2]),
-
-  {TimeM1M1, _} = timer:tc(my_client1, bm, [MyServer1, Count]),
-  io:format("My1-My1: ~p~n", [TimeM1M1]),
-
-  {TimeM1M2, _} = timer:tc(my_client1, bm, [MyServer2, Count]),
-  io:format("My1-My2: ~p~n", [TimeM1M2]),
-
-  {TimeM2M1, _} = timer:tc(my_client2, bm, [MyServer1, Count]),
-  io:format("My2-My1: ~p~n", [TimeM2M1]),
-
-  {TimeM2M2, _} = timer:tc(my_client2, bm, [MyServer2, Count]),
-  io:format("My2-My2: ~p~n", [TimeM2M2]),
 
   GenServer ! stop,
   MyServer1 ! stop,
